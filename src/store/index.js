6fd7f1e153b1e1848bca075d13fd55210d5554d8
dygapp <dygapp@qq.com>
2018-12-06 17:46:40 +0800

    移除store\naf\dict，改为从@lib/store/naf/dict加载

--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,13 @@
+import * as dict from '@lib/store/naf/dict';
+
+export const modules = {
+  naf: {
+    namespaced: true,
+    modules: {
+      dict,
+    },
+  },
+};
 export const state = () => {};
 export const mutations = {};
 export const actions = {};
