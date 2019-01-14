51a2c0be532d76de126ae24bac30d16a68261e7b
dygapp <dygapp@qq.com>
2019-01-14 16:07:56 +0800

    upgrade

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ const frameSrc = path.resolve(__dirname, '../admin-frame');
 
 module.exports = {
   // baseUrl: './',
-  baseUrl: process.env.VUE_APP_ROOT_URL + 'jobs',
+  publicPath: process.env.VUE_APP_ROOT_URL + 'jobs',
 
   configureWebpack: {
     externals: {
