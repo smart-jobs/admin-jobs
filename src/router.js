d67636bab0b3d46e7d1b4759f2ba607877af0624
dygapp <dygapp@qq.com>
2019-03-07 15:21:48 +0800

    change router mode

--- src/router.js
+++ src/router.js
@@ -5,7 +5,7 @@ import Home from './views/home.vue';
 Vue.use(Router);
 
 const router = new Router({
-  // mode: process.env.VUE_APP_ROUTER_MODE || 'history',
+  mode: process.env.VUE_APP_ROUTER_MODE || 'history',
   base: process.env.BASE_URL,
   routes: [
     {
